292506b49f471ee7b908f94a57958674864e2bc9
Ranjeet Kumar <ranjeetnbittu@gmail.com>
2023-02-07 13:02:59 +0530

    added document string for login() method

--- appointment.py
+++ appointment.py
@@ -30,9 +30,28 @@ class MainApp(QMainWindow, ui):
     self.ReportsIcon1.clicked.connect(self.show_reports)
     self.ReportsIcon2.clicked.connect(self.show_reports)
 
-  # below method is used for
-  # Admin Login functionality
   def login(self):
+    """
+    This code block defines a login method. 
+    The method serves to handle the logic for a login process, 
+    checking if the entered password matches a pre-defined value of "admin".
+
+    The method takes self as an argument, 
+    which refers to the instance of the object that is calling the method.
+
+    The password entered by the user is retrieved from the PASSWORD 
+    object using the text() method and stored in the "pw" variable.
+
+    Conditional statement is used to check if the entered 
+    password matches the value of "admin" (hard-coded value).
+
+    If the entered password matches "admin", the following actions take place:
+
+    The PASSWORD object's text is cleared using the setText("") method.
+    The text in the LOGININFO object is cleared using the setText("") method.
+    The current tab in the tabWidget object is set to the 
+    first tab (index 1) using the setCurrentIndex(1) method.
+    """
     pw = self.PASSWORD.text()
     if (pw == "admin"):
       self.PASSWORD.setText("")
